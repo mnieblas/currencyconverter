dfd9eff0e6c18783b560082ed6cce8516875e89c
Marina Nieblas <mnieblas@doctorjekyll.com>
2019-09-05 12:56:43 +0200

    Updates

---
+++ precache-manifest.f40d16f7dac6e323033416173ce46a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1607b36a8842f5cf11d64e963a4aa2",
+    "url": "/currencyconverter/index.html"
+  },
+  {
+    "revision": "fc827b7427bbf00ba40a",
+    "url": "/currencyconverter/static/css/main.e5d1f096.chunk.css"
+  },
+  {
+    "revision": "a201fc70b1c2c60040fa",
+    "url": "/currencyconverter/static/js/2.69abe853.chunk.js"
+  },
+  {
+    "revision": "fc827b7427bbf00ba40a",
+    "url": "/currencyconverter/static/js/main.53f350fb.chunk.js"
+  },
+  {
+    "revision": "55b96e343eed3dd65e6f",
+    "url": "/currencyconverter/static/js/runtime~main.b0faadcd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/currencyconverter/precache-manifest.f40d16f7dac6e323033416173ce46a63.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currencyconverter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
